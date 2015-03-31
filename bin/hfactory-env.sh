481a6420b923f4dc01027c9f85e99a75dcd730f1
Eric Le Blouc'h <eric.leblouch@ubeeko.com>
2015-03-31 19:06:01 +0200

    First version

---
+++ bin/hfactory-env.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+IDS="hbase server"
+MACHINE_NAME=hfactoryserver
+#USE_CONFDIR="-v ${homedir}/conf:/conf"
+
+dm(){
+	docker-machine $@
+}
+
+dmip(){
+  dm ip $MACHINE_NAME 2> /dev/null
+}
+
+dmup(){
+  ign=$(dm start $MACHINE_NAME 2> /dev/null)
+}
+
+dmenv(){
+  dm env ${MACHINE_NAME}
+}
+
+up() {
+  dmup
+  # Use $(up) to initialize docker env
+  echo $(dmenv)
+}
+
+killz(){
+	$(dmenv)
+	echo "Killing hfactory docker containers:"
+	echo $IDS | xargs -n 1 docker kill
+	echo $IDS | xargs -n 1 docker rm
+}
+
+stop(){
+	$(dmenv)
+	echo "Stopping hfactory docker containers:"
+	echo $IDS | xargs -n 1 docker stop
+	echo $IDS | xargs -n 1 docker rm
+}
+
+serverlog(){
+	$(dmenv)
+	echo "Show Server logs"
+	docker logs -f server
+}
+
+hbaselog(){
+	$(dmenv)
+	echo "Show HBase logs"
+	docker logs -f hbase
+}
+
+start(){
+	$(up)
+	homedir=$(dm ssh $MACHINE_NAME pwd)
+	if [ ${PERSISTS:-false} = "true" ]
+		then
+			PERSIST_DATA=${homedir}/data:/data
+	fi
+	echo "Starting HFactory containers"
+	STANDALONEHBASE=$(docker run \
+		-d \
+		-v ${homedir}/share:/share \
+		$PERSIST_DATA \
+		--name hbase \
+		--net host \
+		hfactory/hbase)
+	echo "Started hbase in container $STANDALONEHBASE"
+	HFACTORY_SERVER=$(docker run \
+		-d \
+		-v ${homedir}/apps:/apps \
+		$USE_CONFDIR \
+		--name server \
+		--net host \
+		hfactory/server)
+	echo "Started hfactory server in container $HFACTORY_SERVER"
+}
+
+update(){
+	$(dmenv)
+	git pull
+	echo $IDS | awk -v RS=' ' -v FS='\n' '{ print "hfactory/" $1 ":latest"}' | xargs -n 1 docker pull
+}
+
+upgrade(){
+	dm upgrade $MACHINE_NAME
+	update
+}
+
+copyPath(){
+  scp -i "$HOME/.docker/machine/machines/$MACHINE_NAME/id_rsa" -r $1 docker@$(dmip):$2
+}
+
+case "$1" in
+	init)
+		dm create --driver virtualbox $MACHINE_NAME || echo "Already exists"
+		$(up)
+		update
+		dm ssh $MACHINE_NAME mkdir share data apps conf
+		ip=$(dmip)
+		echo "Update /etc/hosts with the following line:"
+		echo "$ip	hfactoryserver"
+		echo "Then use $0 start to start the hfactory environment and connect to it with the http://hfactoryserver"
+		echo "Use $0 putApp appFolder to add an application and $0 removeApp appName to remove it"
+		;;
+	putApp)
+		if [ -z "$2" ]
+			then
+				dm ssh $MACHINE_NAME rm -rf /apps/$2
+				copyPath $2 apps
+		fi
+		;;
+	removeApp)
+		if [ -z "$2" ]
+			then
+				dm ssh $MACHINE_NAME rm -rf /apps/$2
+		fi
+		;;
+#	updateConf)
+#		copyPath ${2:-conf}'/*' conf
+#		;;
+	up)
+		dmup
+		;;
+	shutdown)
+		dm stop $MACHINE_NAME
+		;;
+	restart)
+		stop
+		start
+		;;
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	hbaselog)
+		hbaselog
+		;;
+	serverlog)
+		serverlog
+		;;
+	kill)
+		killz
+		;;
+	upgrade)
+		upgrade
+		;;
+	update)
+		update
+		;;
+	ssh)
+		dm ssh
+		;;
+	status)
+		$(dmenv)
+		docker ps
+		;;
+	*)
+		echo "Usage: $0 {start|stop|hbaselog|serverlog|kill|update|upgrade|restart|status|up|shutdown|ssh}"
+		echo "Also:  $0 putApp appFolder"
+		echo "       $0 removeApp appName"
+		RETVAL=1
+esac
+
